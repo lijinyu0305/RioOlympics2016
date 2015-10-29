9f06cfe44f3c7778706afd3a23fd051993f5dfaa
macbook <macbook@founder.local>
2015-10-29 16:31:31 +0800

    新项目加入

---
+++ PersistenceLayer/PersistenceLayer/PersistenceLayer.h
@@ -0,0 +1,19 @@
+//
+//  PersistenceLayer.h
+//  PersistenceLayer
+//
+//  Created by Ethan Su on 15/10/29.
+//  Copyright © 2015年 Founder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PersistenceLayer.
+FOUNDATION_EXPORT double PersistenceLayerVersionNumber;
+
+//! Project version string for PersistenceLayer.
+FOUNDATION_EXPORT const unsigned char PersistenceLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PersistenceLayer/PublicHeader.h>
+
+
---
+++ RioOlympics2016/RioOlympics2016/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RioOlympics2016
+//
+//  Created by Ethan Su on 15/10/29.
+//  Copyright © 2015年 Founder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
